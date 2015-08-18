a6846cb196e8e97f43722b85f333382cae48a95e
Rodrigo Peplau <rodrigo@peplau.com.br>
2015-08-18 16:42:15 -0300

    Action: Replace string on item name

---
+++ Rules.Processor.Api/Extensions/StringExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace Rules.Processor.Api.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string ReplaceQuotesForSpecialChars(this string str)
+        {
+            if (str[0] != '"' || str[str.Length - 1] != '"')
+                return str;
+            str = str.Substring(1);
+            str = str.Substring(0, str.Length - 1);
+            return str;
+        }
+    }
+}
--- Rules.Processor.Api/Rules/Actions/ReplaceStringInItemName.cs
+++ Rules.Processor.Api/Rules/Actions/ReplaceStringInItemName.cs
@@ -5,40 +5,35 @@ using Sitecore.SecurityModel;
 
 namespace Rules.Processor.Api.Rules.Actions
 {
-    public class ReplaceStringInField<T> : RuleAction<T> where T : RuleContext
+    class ReplaceStringInItemName<T> : RuleAction<T> where T : RuleContext
     {
         public string String1 { get; set; }
         public string String2 { get; set; }
-        public string Name { get; set; }
 
         public override void Apply(T ruleContext)
         {
             // Scape in case of empty params
-            if (string.IsNullOrEmpty(String1) || string.IsNullOrEmpty(String2) || string.IsNullOrEmpty(Name))
+            if (string.IsNullOrEmpty(String1) || string.IsNullOrEmpty(String2))
                 return;
 
             String1 = String1.ReplaceQuotesForSpecialChars();
             String2 = String2.ReplaceQuotesForSpecialChars();
 
-            // Scape if field does not exists or has no val
             var item = ruleContext.Item;
-            var field = item.Fields[Name];
-            if (field == null || !field.HasValue)
-                return;
 
-            // Do replacement on memory
-            var fieldVal = field.Value;
-            var replaced = fieldVal.Replace(String1, String2);
+            // Replace on memory
+            var currentName = item.Name;
+            var targetName = currentName.Replace(String1, String2);
 
-            // Scape if value is the same
-            if (fieldVal == replaced)
+            // Scape if name does not changed
+            if (currentName == targetName)
                 return;
 
             // Save updated val
             using (new SecurityDisabler())
             {
                 item.Editing.BeginEdit();
-                item.Fields[Name].SetValue(replaced, false);
+                item.Name = targetName;
                 item.Editing.EndEdit();
             }
         }
