f9a4b88dea26a37f5d33f573e05408856dd07261
Rodrigo Peplau <rodrigo@peplau.com.br>
2015-08-18 14:20:58 -0300

    Action: Serialize item

---
+++ Rules.Processor.Api/Rules/Actions/SerializeItem.cs
@@ -0,0 +1,14 @@
+﻿using Sitecore.Data.Serialization;
+using Sitecore.Rules;
+using Sitecore.Rules.Actions;
+namespace Rules.Processor.Api.Rules.Actions
+{
+    public class SerializeItem<T> : RuleAction<T> where T : RuleContext
+    {
+        public override void Apply(T ruleContext)
+        {
+            var item = ruleContext.Item;
+            Manager.DumpItem(item);
+        }
+    }
+}
