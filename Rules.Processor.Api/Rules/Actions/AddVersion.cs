3c0165947174b2cb5991476d2eb3d151f7ba275f
Rodrigo Peplau <rodrigo@peplau.com.br>
2015-09-02 17:03:27 -0300

    Action: Change template

---
+++ Rules.Processor.Api/Rules/Actions/AddVersion.cs
@@ -0,0 +1,32 @@
+﻿using Sitecore.Data.Managers;
+using Sitecore.Rules;
+using Sitecore.Rules.Actions;
+using Sitecore.SecurityModel;
+
+namespace Rules.Processor.Api.Rules.Actions
+{
+    class AddVersion<T> : RuleAction<T> where T : RuleContext
+    {
+        public string Language { get; set; }
+
+        public override void Apply(T ruleContext)
+        {
+            // Scape in case of empty params
+            if (string.IsNullOrEmpty(Language))
+                return;
+
+            // Scape if language does not exists
+            var item = ruleContext.Item;
+
+            // Add Version
+            var language = LanguageManager.GetLanguage(Language);
+            var localizedItem = item.Database.GetItem(item.ID, language);
+            using (new SecurityDisabler())
+            {
+                localizedItem.Editing.BeginEdit();
+                localizedItem.Versions.AddVersion();
+                localizedItem.Editing.EndEdit();
+            }
+        }
+    }
+}
