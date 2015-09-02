b644cb31b27fdaca2f02381069f18fc475cf6757
Rodrigo Peplau <rodrigo@peplau.com.br>
2015-09-02 15:26:27 -0300

    New Action: Delete Item

---
+++ Rules.Processor.Api/Rules/Actions/DeleteItem.cs
@@ -0,0 +1,19 @@
+﻿using Sitecore.Rules;
+using Sitecore.Rules.Actions;
+using Sitecore.SecurityModel;
+
+namespace Rules.Processor.Api.Rules.Actions
+{
+    class DeleteItem<T> : RuleAction<T> where T : RuleContext
+    {
+        public override void Apply(T ruleContext)
+        {
+            var item = ruleContext.Item;
+            using (new SecurityDisabler())
+            {
+                item.DeleteChildren();
+                item.Delete();
+            }
+        }
+    }
+}
