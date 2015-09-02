d19318f996dbedd37b01c3fea6009cb40d6b35f0
Rodrigo Peplau <rodrigo@peplau.com.br>
2015-09-02 15:57:55 -0300

    Action: Copy/Move/Clone to...

---
+++ Rules.Processor.Api/Rules/Actions/CopyItemTo.cs
@@ -0,0 +1,43 @@
+﻿using Sitecore.Data;
+using Sitecore.Rules;
+using Sitecore.Rules.Actions;
+using Sitecore.SecurityModel;
+
+namespace Rules.Processor.Api.Rules.Actions
+{
+    class CopyItemTo<T> : RuleAction<T> where T : RuleContext
+    {
+        public string Copy { get; set; }
+        public string ItemId { get; set; }
+
+        public override void Apply(T ruleContext)
+        {
+            // Scape in case of empty params
+            if (string.IsNullOrEmpty(ItemId) || string.IsNullOrEmpty(Copy))
+                return;
+
+            // Scape if target item does not exists
+            var item = ruleContext.Item;
+            var targetItem = item.Database.GetItem(new ID(ItemId));
+            if (targetItem == null)
+                return;
+
+            // Copy item to target
+            using (new SecurityDisabler())
+            {
+                switch (Copy.ToLower())
+                {
+                    case "clone":
+                        item.CloneTo(targetItem);
+                        break;
+                    case "copy":
+                        item.CopyTo(targetItem, item.Name);
+                        break;
+                    case "move":
+                        item.MoveTo(targetItem);
+                        break;
+                }
+            }
+        }
+    }
+}
